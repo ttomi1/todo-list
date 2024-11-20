4c6fba3879bdb5a12e398bbf72d26250cc3ddd6d
ttomi1 <tomi.zir@gmail.com>
2024-11-20 12:04:35 +0100

    edit comment

--- todo-list-project/src/app/app.component.ts
+++ todo-list-project/src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'todo-list-project';
+  username: string = '';
+  comment: string = '';
+  comments: { username: string; text: string; isEditing: boolean }[] = [];
+
+  // Add a new comment
+  addComment() {
+    if (this.username.trim() && this.comment.trim()) {
+      this.comments.push({
+        username: this.username,
+        text: this.comment,
+        isEditing: false
+      });
+      this.username = ''; // Clear input fields
+      this.comment = '';
+    } else {
+      alert('Please fill in both fields!');
+    }
+  }
+
+  // Enable editing for a specific comment
+  editComment(index: number) {
+    this.comments[index].isEditing = true;
+  }
+
+  // Save edited comment
+  saveComment(index: number) {
+    this.comments[index].isEditing = false;
+  }
 }
